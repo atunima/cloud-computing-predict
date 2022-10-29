1a06007f7486b3dc086724a5f562de1bc146b0ad
Atunima <atunima@yahoo.com>
2022-10-29 15:01:42 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://rc69ly11lk.execute-api.eu-west-1.amazonaws.com/default/cloudPredict",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
